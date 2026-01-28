eff401b06280ac157751bba4d36e775cc8d62ffa
cin <1316108264@qq.com>
2026-01-28 13:57:24 +0800

    feat:前端url图片上传开发完成

---
+++ cin-picture-frontend/src/components/UrlPictureUpload.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="url-picture-upload">
+    <a-input-group compact style="margin-bottom: 16px">
+      <a-input
+        v-model:value="fileUrl"
+        style="width: calc(100% - 120px)"
+        placeholder="请输入图片 URL"
+      />
+      <a-button type="primary" :loading="loading" @click="handleUpload" style="width: 120px"
+        >提交</a-button
+      >
+    </a-input-group>
+    <img v-if="picture?.url" :src="picture?.url" alt="avatar" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { uploadPictureByUrlUsingPost } from '@/api/pictureController'
+import { message } from 'ant-design-vue'
+import { ref } from 'vue'
+
+interface Props {
+  picture?: API.PictureVO
+  onSuccess?: (newPicture: API.PictureVO) => void
+}
+
+const props = defineProps<Props>()
+// props 是 Vue3 <script setup> 里组件“接收父组件传参”的对象 ，来源是 defineProps()
+const loading = ref<boolean>(false)
+const fileUrl = ref<string>('')
+
+const handleUpload = async () => {
+  loading.value = true
+  try {
+    const params: API.PictureUploadRequest = { fileUrl: fileUrl.value }
+    if (props.picture) {
+      params.id = props.picture.id
+    }
+    const res = await uploadPictureByUrlUsingPost(params)
+    if (res.data.code === 0 && res.data.data) {
+      message.success('图片上传成功')
+      // 将上传成功的图片信息传递给父组件
+      props.onSuccess?.(res.data.data)
+    } else {
+      message.error('图片上传失败，' + res.data.message)
+    }
+  } catch (error) {
+    message.error('图片上传失败')
+  } finally {
+    loading.value = false
+  }
+}
+</script>
--- cin-picture-frontend/src/pages/AddPicturePage.vue
+++ cin-picture-frontend/src/pages/AddPicturePage.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
-import { editPictureUsingPost, listPictureTagCategoryUsingGet,getPictureVoByIdUsingGet } from '@/api/pictureController'
+import {
+  editPictureUsingPost,
+  listPictureTagCategoryUsingGet,
+  getPictureVoByIdUsingGet,
+} from '@/api/pictureController'
 import PictureUpload from '@/components/PictureUpload.vue'
+import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
 import { message } from 'ant-design-vue'
 import { computed, onMounted, reactive, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
@@ -11,7 +16,7 @@ const picture = ref<API.PictureVO>()
 const pictureForm = reactive<API.PictureEditRequest>({})
 const categoryOptions = ref<Option[]>([])
 const tagOptions = ref<Option[]>([])
-
+const uploadType = ref<'file' | 'url'>('file')
 const onSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture
   pictureForm.name = newPicture.name
@@ -96,7 +101,16 @@ onMounted(() => {
     <h2 style="margin-bottom: 16px">
       {{ isEdit ? '修改图片' : '创建图片' }}
     </h2>
-    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+    <!-- 选择上传方式 -->
+    <a-tabs v-model:activeKey="uploadType"
+      >>
+      <a-tab-pane key="file" tab="文件上传">
+        <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+      </a-tab-pane>
+      <a-tab-pane key="url" tab="URL 上传" force-render>
+        <UrlPictureUpload :picture="picture" :onSuccess="onSuccess" />
+      </a-tab-pane>
+    </a-tabs>
     <a-form v-if="picture" layout="vertical" :model="pictureForm" @finish="handleSubmit">
       <a-form-item label="名称" name="name">
         <a-input v-model:value="pictureForm.name" placeholder="请输入名称" />
